39cee9b17b91f7cdfe505dba2032b75d0255ae98
Kelly Tran <kt457@cornell.edu>
2024-10-05 18:01:02 -0400

    merge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="index" options={{ headerShown: false }} />
+          {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
           {/* <Stack.Screen name="search/[query]" options={{ headerShown: false }} /> */}
           <Stack.Screen name="onboarding" options={{ presentation: 'fullScreenModal', headerShown: false }} />
           <Stack.Screen name="+not-found" />
