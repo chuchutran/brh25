fb3c105c951b874708dea5d5f2526e9febb26a6c
dorawengg <cw749@cornell.edu>
2024-10-05 19:32:11 -0400

    merge problemo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { ThemedText } from '@/components/ThemedText';
 import { Redirect, router } from 'expo-router';
 import CustomButton from '@/components/CustomButton';
+import { FloatingActionButton } from '@/components/FloatingActionButton';
 
 //images - fix later
 const mooDengImage = require('../../assets/images/moo_deng.png');
@@ -14,20 +15,22 @@ export default function Inventory() {
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={{ height: '100%' }}>
         <View className="w-full justify-center items-center h-full px-4">
-          <Image 
-            source={mooDengImage} 
-            className="w-[130px] h-[84px]" 
+          <Image
+            source={mooDengImage}
+            className="w-[130px] h-[84px]"
             resizeMode="contain"
-            />
+          />
           <ThemedText className="text-3xl font-semibold">Welcome to Moo Deng</ThemedText>
-          
+
           {/* Contine with Email button */}
           <CustomButton
             title="Continue with Email"
             handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
+          <FloatingActionButton />
         </View>
+
       </ScrollView>
       {/* <StatusBar backgroundColor="#161622" style="light" /> */}
     </SafeAreaView>
